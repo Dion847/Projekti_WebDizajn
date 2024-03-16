a388046dc1c48644a787077eee75c74152a69685
Dion847 <dion.valla02@gmail.com>
2024-03-16 23:48:56 +0100

    Lidhja me databaaze

---
+++ Database.php
@@ -0,0 +1,33 @@
+<?php
+
+
+use PDO, PDOException;
+
+
+abstract class Database
+{
+    private $server = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $db = "RestaurantFH";
+
+    public function connectDB()
+    {
+        try {
+            $conn = "mysql:host=" . $this->server . ";dbname=" . $this->db;
+
+            $pdo = new PDO($conn, $this->user, $this->password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $pdo;
+        } catch (PDOException $e) {
+            echo "Database connection failed" . $e->getMessage();
+            return null;
+        }
+    }
+
+    public function prepare($sql)
+    {
+        return $this->connectDB()->prepare($sql);
+    }
+}
+?>

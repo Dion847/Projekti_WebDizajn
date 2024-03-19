a5c430847f6cbcb29940cb2f6b1fb1a7beea01d0
Dion847 <dion.valla02@gmail.com>
2024-03-19 23:06:23 +0100

    rregullim i folderave

---
+++ inc/header.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="Css/Style.css">
+<title>Flavor Haven Restaurant</title>
+<header>
+        <div class="headeri">
+            <div class="name"> 
+                <p>Flavor Haven Restaurant</p>
+            </div>
+        </div>
+        <ul>
+            <li><a style="text-decoration:none; color: black;" href="index.php">Home</a></li>
+            <li><a style="text-decoration:none; color: black;" href="News.php">News</a></li>
+            <li><a style="text-decoration:none; color: black;" href="Menu.php">Menu</a></li>
+            <li><a style="text-decoration:none; color: black;" href="aboutus.php">About Us</a></li>
+            <li><a style="text-decoration:none; color: black;" href="contactus.php">Contact Us</a></li>
+            <li><a style="text-decoration:none; color: black;" href="login.php">Log In</a></li>
+        </ul>
+</header>

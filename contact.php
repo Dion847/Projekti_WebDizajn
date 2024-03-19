8183fd9f3dcf701252eab226dabf61833713d8f1
Dion847 <dion.valla02@gmail.com>
2024-03-19 23:08:53 +0100

    contact form

---
+++ contact.php
@@ -0,0 +1,34 @@
+<?php 
+include("inc/header.php");
+
+if(isset($_POST['submit'])){
+    $contact = new Admin\Lib\Contact();
+    $contact->setEmail($_POST['email']);
+    $contact->setMessage($_POST['message']);
+
+    if($contact->createContact()){
+        echo "<script>alert('Message successfully sent!')</script>";
+    }else{
+        echo "<script>alert('Message failed to send!')</script>";
+    }
+}
+?>
+<link rel="stylesheet" href="Css/contactForm">
+<div class="container">
+    <div class="contactBox">
+        <h1>Contact Us</h1>
+        <form action="" method="post">
+
+            <!-- <label for="email">Email</label> -->
+            <input type="email" name="email" id="email" placeholder="Email" value="<?php if (!empty($_SESSION['email'])) {
+                echo $_SESSION['email'];
+            } ?>" required>
+            <div id="errorEmail" class="error"></div>
+            <!-- <label for="password">Password</label> -->
+            <input type="text" name="message" id="message" placeholder="Contact Us!" required>
+            <div id="errorDescription" class="error"></div>
+
+            <input type="submit" name="submit" onclick="contactValidation()">
+        </form>
+    </div>
+</div>
